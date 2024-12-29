515b6f904c039c64394fa42331cfefd0bc067034
Abdul Muhaymin <abdul.muhaymin@bilkent.edu.tr>
2024-12-30 01:32:31 +0300

    pushing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,33 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import sub.single_fe as fe
+import sub.multiple_fe as mfe
+import sub.siesta_input as siesta_in
+import sub.analysis as analysis
+
+def main():
+    st.set_page_config(page_title="Isolated Defect Utility", page_icon="💎")        
+    st.title("💎 Isolated Defect Utility")
+
+    options = [
+        ("Plot defect formation energy (FE) diagram for a single defect", "fe"),
+        ("Plot defect FE diagram for multiple defects ", "mfe"),
+        ("Generate an input file for defect calculation in SIESTA", "siesta_in"),
+        ("Analyze your defect calculation results", "analysis")
+    ]
+
+    page = st.radio("Choose a task:", options=[opt[0] for opt in options])
+    page = next(opt[1] for opt in options if opt[0] == page)
+
+    if page == "fe":
+        fe.main()
+    elif page == "mfe":
+        mfe.main()
+    elif page == "siesta_in":
+        siesta_in.main()
+    elif page == "analysis":
+        analysis.main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sub/siesta_input.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def main():
+    st.title("SIESTA Defect Input Generator")
+    st.header("Coming soon...")
